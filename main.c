7ccc701ae92b80e0305c587e7a6b41285e7fb5ef
Richard <54506990+RichLarIva@users.noreply.github.com>
2026-02-13 20:10:40 +0100

    Fully implemented the counting of the throws

--- main.c
+++ main.c
@@ -8,6 +8,25 @@ int main()
 
    
     // Your code goes here
+    int rolls[100];
+    int countDice[6] = {0};
+    int sum = 0;
+
+    for (int i = 0; i < 100; i++)
+    {
+        // plus 1 to get rid of 0 since how modulo works
+        int diceThrow = (rand() % 6) + 1;
+        rolls[i] = diceThrow;
+        countDice[diceThrow - 1]++;
+
+        sum+=diceThrow;
+    }
+
+    // Write the total for each number 1-6
+    for (int i = 0; i < 6; i++)
+    {
+        printf("%d: %d\n", i+1, countDice[i]);
+    }
 
     return 0;
 }
